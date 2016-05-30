8392536d6489512939f4b49e5be7f59dc241f2cc
yancey1204 <yancey1204@gmail.com>
2016-05-30 18:50:45 +0800

    add sqlite

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const PORT = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
 
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
---
+++ userService/userController.js
@@ -0,0 +1,15 @@
+class User {
+  constructor() {
+    this.display_name = null;
+    this.username = null;
+    this.password = null;
+    this.eexternal_token = null;
+    this.external_source = null;
+    this.token = null;
+  }
+  createUser() {
+    return true;
+  }
+}
+
+export { User };
